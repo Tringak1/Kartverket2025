fd4d7092a4793f59d5cb1b93dec88b8fb5baad89
Tringak1 <tringakongjeli@hotmail.com>
2025-02-21 13:29:16 +0100

    Enhanced cookie security in program.cs (enabled httponly and secure cookies), removed unused verifyemail reference in changepassword.cshtml, added required field validation message in loginviewmodel.cs

--- Nettside/Models/ViewModel/LoginViewModel.cs
+++ Nettside/Models/ViewModel/LoginViewModel.cs
@@ -4,6 +4,7 @@ namespace Nettside.Models.ViewModel
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage = "Username is required.")]
         public string UserName { get; set; }
 
         [Required(ErrorMessage = "Password is required.")]
