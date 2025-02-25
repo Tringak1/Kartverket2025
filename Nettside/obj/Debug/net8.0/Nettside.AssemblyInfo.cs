ce21fcbfae79f7c8eb104c311ea9de0ecd92165e
Tringak1 <tringakongjeli@hotmail.com>
2025-02-25 17:42:56 +0100

    innførte unit testing

--- Nettside/obj/Debug/net8.0/Nettside.AssemblyInfo.cs
+++ Nettside/obj/Debug/net8.0/Nettside.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nettside")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6f1a97f81303e63b1a8aba737a0ec3ceea99708")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b259fbe197f74f4422629bd5b381a53c0874129")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nettside")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nettside")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
